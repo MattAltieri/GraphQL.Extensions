8188fbffd67061596eb12578683cc16581ba3139
Matt Altieri <matthew.altieri@macys.com>
2018-11-14 11:14:27 -0500

    Completed SlicerGraphType.

--- src/GraphQL.Extensions/GraphQL.Types/Pagination/SlicerGraphType.cs
+++ src/GraphQL.Extensions/GraphQL.Types/Pagination/SlicerGraphType.cs
@@ -4,5 +4,13 @@ using GraphQL.Types;
 namespace GraphQL.Types.Pagination {
     public class SlicerGraphType : ObjectGraphType<Slicer> {
         
+        public SlicerGraphType() {
+
+            Name = "Slicer";
+            Field<NonNullGraphType<BooleanGraphType>>("hasNextPage");
+            Field<NonNullGraphType<BooleanGraphType>>("hasPreviousPage");
+            Field<StringGraphType>("startCursor");
+            Field<StringGraphType>("endCursor");
+        }
     }
 }
