b66e4091c29258dd1d13ceecb71c679be6cc2541
Matt Altieri <M983801@federated.fds>
2018-11-16 10:41:46 -0500

    Implemented test cases for OrderByColumn constructor.

---
+++ test/GraphQL.Extensions.Test/MockEntity.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GraphQL.Extensions.Test {
+    public class MockEntity {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
